5f6f4ee9051eacab88f45170e16332c825de60d8
Lakshit Vedant <lakshitvedant@Lakshits-MacBook-Air.local>
2025-01-17 17:39:10 +0530

    cleanup

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import SidebarItem from "@/components/Sidebar/SidebarItem";
